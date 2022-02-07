8ebd81090b831ab9193400d5b3c22dfd07b949fd
Saeed Nemati <sn.1361@gmail.com>
2022-02-07 21:11:54 +0330

    committed by script

---
+++ Api/Admin/HierarchyController.cs
@@ -0,0 +1,8 @@
+// namespace Taxonomy;
+
+// public class HierarchyController : Controller<HierarchyView, Hierarchy>
+// {
+//     public override ReadBusiness<HierarchyView> ReadBusiness => new HierarchyBusiness();
+
+//     public override Business<HierarchyView, Hierarchy> Business => new HierarchyBusiness();
+// }
