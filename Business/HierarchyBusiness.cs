b809928dcd205e8e24bf1be0b503da88f0255b98
Saeed Nemati <sn.1361@gmail.com>
2022-02-14 10:37:10 +0330

    committed by script

--- Business/HierarchyBusiness.cs
+++ Business/HierarchyBusiness.cs
@@ -5,4 +5,14 @@ public class HierarchyBusiness : TreeBusiness<HierarchyView, Hierarchy>
     protected override Read<HierarchyView> Read => Repository.HierarchyView;
 
     protected override Write<Hierarchy> Write => Repository.Hierarchy;
+
+    public HierarchyView Create(string entityType, string title)
+    {
+        var entityTypeGuid = new EntityTypeBusiness().GetGuid(entityType);
+        var hierarchy = new Hierarchy();
+        hierarchy.EntityTypeGuid = entityTypeGuid;
+        hierarchy.Title = title;
+        Create(hierarchy);
+        return Get(hierarchy.Id);
+    }
 }
