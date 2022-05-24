dc70a041eec84f81c7f852ee3369ec9a732114ce
Saeed Nemati <sn.1361@gmail.com>
2022-05-24 13:52:29 +0430

    committed by script

--- Api/Admin/HierarchyController.cs
+++ Api/Admin/HierarchyController.cs
@@ -5,4 +5,18 @@ public class HierarchyController : TreeController<HierarchyView, Hierarchy>
     public override ReadBusiness<HierarchyView> ReadBusiness => new HierarchyBusiness();
 
     public override TreeBusiness<HierarchyView, Hierarchy> Business => new HierarchyBusiness();
+
+    [BindProperty(SupportsGet=true)]
+    public string EntityType { set; set; }
+
+    protected override void PreCreation(Hierarchy model)
+    {
+        if (EntityType.IsNothing())
+        {
+            throw new ClientException("EntityType is not provided");
+        }
+        var entityTypeGuid = new EntityTypeBusiness().GetGuid(EntityType);
+        model.EntityTypeGuid = entityTypeGuid;
+        base.PreCreation(model);
+    }
 }
