ca33661b7b6658e2c859e0ae7dd62699e6147dff
Saeed Nemati <sn.1361@gmail.com>
2022-07-25 19:28:22 +0430

    committed by script

--- Api/Admin/HierarchyController.cs
+++ Api/Admin/HierarchyController.cs
@@ -9,6 +9,16 @@ public class HierarchyController : TreeController<HierarchyView, Hierarchy>
     [BindProperty(SupportsGet = true)]
     public string EntityType { get; set; }
 
+    public override Action<ListParameters> ListParametersAugmenter => listParameters =>
+    {
+        if (EntityType.IsNothing())
+        {
+            throw new ClientException("EntityType is not provided");
+        }
+        var entityTypeGuid = new EntityTypeBusiness().GetGuid(EntityType);
+        listParameters.AddFilter<HierarchyView>(i => i.EntityTypeGuid, entityTypeGuid);
+    };
+
     public override Action<Hierarchy> PreCreation => model =>
     {
         if (EntityType.IsNothing())
